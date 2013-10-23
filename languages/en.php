e4c05c7694b91427a47f0eca42dc8f9509e50acc
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2013-10-23 13:12:01 +0300

    Added english translations.

---
+++ languages/en.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * English translations file
+ */
+
+$lang = array(
+	'pdf_export:admin_notice:dir_creation_failed' => 'Failed to create the directory %s. Check data root write permissions.',
+	'pdf_export:canvas_not_supported' => 'Your Internet browser does not support this feature',
+	'pdf_export:date_format' => 'j F Y',
+);
+
+add_translation('en', $lang);
