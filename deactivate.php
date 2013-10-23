839e95f2e8f9a0a30f65663fc603972d66f4950b
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2013-10-23 14:02:31 +0300

    Delete the tmp dir for pdf exports when plugin is disabled.

--- deactivate.php
+++ deactivate.php
@@ -1,4 +1,22 @@
 <?php
 /**
- * @todo Delete the whole pdf tmp dir?
+ * Delete the temporary directory for pdf exports
  */
+
+$file_dir = elgg_get_data_path() . 'pdf_export';
+
+if (is_dir($file_dir)) {
+	$files = scandir($file_dir);
+
+	// Remove the directory contents
+	foreach ($files as $filename) {
+		if ($filename == '.' || $filename == '..') {
+			continue;
+		}
+
+		unlink("{$file_dir}/{$filename}");
+	}
+
+	// Remove the directory
+	rmdir($file_dir);
+}
